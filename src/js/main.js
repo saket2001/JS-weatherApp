fc448092b47927f7e8642969bd9a58c272302e4f
saket sunil shalaka chandorkar <54675560+saket2001@users.noreply.github.com>
2021-05-25 21:34:02 +0530

    4 th commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
 ///////////////////
 const API_KEY = `3bcee8d340b7eb85f4f2ffb2e4c646b8`;
 const mainContainer = document.querySelector("main");
+const form = document.querySelector(".form");
+const CityInput = document.querySelector("#city_input");
+const searchBtn = document.querySelector("#submit_btn");
 const weather_container = document.querySelector(".weather__container");
 const displayDate = document.querySelector(".current_date");
 const feelsLikeTemp = document.querySelector(".weather__feels");
@@ -16,43 +19,35 @@ const windSpeed = document.querySelector(".windspeed");
 const cloudPer = document.querySelector(".cloudPer");
 const humidity = document.querySelector(".humidity");
 const visibility = document.querySelector(".visibility");
+const airQuality = document.querySelector(".airQuality");
+const uvQuality = document.querySelector(".uvQuality");
 
 ///////////////////
 
 let now = moment();
 
 const formatDate = function (d) {
-  return `${d.format("dddd")} ${d.format("MMMM Do YYYY")}`;
-};
-
-const fetchData = async function (url) {
-  try {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
-
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    throw err;
-  }
+  return ` ${d.format("MMMM Do YYYY")}`;
 };
 
 const setData = function (data) {
   // date
   displayDate.innerHTML = formatDate(now);
   // city name
-  user_location.innerHTML = data.timezone;
+  user_location.innerHTML = `${data.location.country},${data.location.name}`;
   // current weather
-  climate.innerHTML = Math.round(data.current.temp) - 270 + "  &#8451";
+  climate.innerHTML = `${data.current.temp_c}  &#8451 `;
+  // climate status
+  climate_status.innerHTML = `${data.current.condition.text}`;
   // feels like
-  feelsLikeTemp.innerHTML = `Feels like  ${
-    Math.round(data.current.feels_like) - 270 + "  &#8451"
-  }`;
+  feelsLikeTemp.innerHTML = `Feels like ${data.current.feelslike_c} &#8451`;
   // other details
-  cloudPer.innerHTML = `Clouds ${data.current.clouds} %`;
-  windSpeed.innerHTML = `Wind speed ${data.current.wind_speed} m/s`;
+  cloudPer.innerHTML = `Clouds ${data.current.cloud} %`;
+  windSpeed.innerHTML = `Wind ${data.current.wind_kph} m/s`;
   humidity.innerHTML = `Humidity ${data.current.humidity} %`;
-  visibility.innerHTML = `Visibility ${data.current.visibility * 0.001} km `;
+  visibility.innerHTML = `Visibility ${data.current.vis_km} km `;
+  airQuality.innerHTML = `Air level ${data.current.air_quality["gb-defra-index"]}`;
+  uvQuality.innerHTML = `Uv level ${data.current.uv}`;
 
   // for upcoming days
 
@@ -60,19 +55,11 @@ const setData = function (data) {
     block.innerHTML = formatDate(moment().add(i + 1, "days"));
   });
   nextDayClimate.forEach((block, i) => {
-    block.innerHTML = Math.round(data.daily[i++].temp.day) - 270 + "  &#8451";
+    block.innerHTML = `${data.forecast.forecastday[i].day.maxtemp_c} &#8451`;
   });
 
-  //
-  if (data.current.weather[0]["main"] === "Clouds") {
-    weatherLogo.setAttribute = `http://openweathermap.org/img/wn/${data.current.weather[0]["icon"]}@2x.png`;
-    climate_status.innerHTML = data.current.weather[0]["description"];
-  }
-
-  if (data.current.weather[0]["main"] === "Rains") {
-    weatherLogo.setAttribute = `http://openweathermap.org/img/wn/${data.current.weather[0]["icon"]}@2x.png`;
-    climate_status.innerHTML = data.current.weather[0]["description"];
-  }
+  //weather logo
+  weatherLogo.src = `http://${data.current.condition.icon.slice(2)}`;
 };
 
 const loadErrorMsg = function (msg) {
@@ -83,48 +70,60 @@ const loadErrorMsg = function (msg) {
     </div>
   `;
   weather_container.style.display = "none";
+  form.style.display = "none";
   mainContainer.insertAdjacentHTML("afterbegin", markup);
 };
 
-const loadData = async function (position) {
+// const loadData = async function (position) {
+//   try {
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
+
+//     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=${API_KEY}`;
+
+//     const data = await fetchData(url);
+
+//     // console.log(data);
+//     // setting data
+//     setData(data);
+//   } catch (err) {
+//     loadErrorMsg(err);
+//   }
+// };
+
+// const currPositionWeather = function () {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(loadData, function () {
+//       loadErrorMsg(
+//         `OOPS!! Not able to get your location<br>Try Again with your location ON`
+//       );
+//     });
+//   }
+// };
+
+const getData = async function (cityName) {
   try {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
+    if (!cityName) throw new Error("Please type a valid city name");
 
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly&appid=${API_KEY}`;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=18fdeb53a3a44ea5a21131705212505&q=${cityName}&days=2&aqi=yes&alerts=no`;
 
-    const data = await fetchData(url);
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    const data = await res.json();
 
-    // setting data
+    console.log(data);
     setData(data);
   } catch (err) {
     loadErrorMsg(err);
   }
 };
-
-const currPositionWeather = function () {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(loadData, function () {
-      loadErrorMsg(
-        `OOPS!! Not able to get your location<br>Try Again with your location ON`
-      );
-    });
-  }
-};
-
-// const getData = async function () {
-//   try {
-//     const cityname = (await CityInput.value) || "Mumbai";
-
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${API_KEY}`;
-//     const data = await fetchData(url);
-
-//     console.log(data);
-//     setData(data);
-//   } catch (err) {
-//     alert(err);
-//   }
-// };
-
 //////////////////////////////
-currPositionWeather();
+// window.addEventListener("load", currPositionWeather);
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  getData(`${CityInput.value}`);
+});
